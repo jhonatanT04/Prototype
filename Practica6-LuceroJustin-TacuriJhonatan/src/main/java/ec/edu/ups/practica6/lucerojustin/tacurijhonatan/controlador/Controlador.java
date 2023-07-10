85d6171687d1e6dce9004f557706cd383b68fde8
Justin Lucero <justinluceroxd04@gmail.com>
2023-07-09 20:46:10 -0500

    Mi parte

--- Practica6-LuceroJustin-TacuriJhonatan/src/main/java/ec/edu/ups/practica6/lucerojustin/tacurijhonatan/controlador/Controlador.java
+++ Practica6-LuceroJustin-TacuriJhonatan/src/main/java/ec/edu/ups/practica6/lucerojustin/tacurijhonatan/controlador/Controlador.java
@@ -14,6 +14,6 @@ import ec.edu.ups.practica6.lucerojustin.tacurijhonatan.modelo.Directorio;
 public class Controlador {
     private Directorio directorio;
     //vales vrg
-    
+    //////////gfjdjgjfdgjfkjdg
 }
 
