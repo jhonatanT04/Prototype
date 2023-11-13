35c6b063d542c81a81eb773d7cd9dc8373a59c58
venot <venotacu@gmail.com">
2023-11-13 14:53:55 -0500

    Mi parte(Jhonatan)

--- LuceroJM-TacuriJF-Prototype/src/main/java/ec/edu/ups/lucerojm/tacurijf/prototype/LuceroJMTacuriJFPrototype.java
+++ LuceroJM-TacuriJF-Prototype/src/main/java/ec/edu/ups/lucerojm/tacurijf/prototype/LuceroJMTacuriJFPrototype.java
@@ -11,6 +11,6 @@ package ec.edu.ups.lucerojm.tacurijf.prototype;
 public class LuceroJMTacuriJFPrototype {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
