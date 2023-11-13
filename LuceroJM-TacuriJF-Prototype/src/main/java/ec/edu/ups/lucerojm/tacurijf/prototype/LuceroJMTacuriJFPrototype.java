bba84f548488ab79f647690741a263c18773f2f8
venot <venotacu@gmail.com">
2023-11-13 14:53:24 -0500

    Mi parte(Jhonatan)

---
+++ LuceroJM-TacuriJF-Prototype/src/main/java/ec/edu/ups/lucerojm/tacurijf/prototype/LuceroJMTacuriJFPrototype.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.lucerojm.tacurijf.prototype;
+
+/**
+ *
+ * @author venot
+ */
+public class LuceroJMTacuriJFPrototype {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
